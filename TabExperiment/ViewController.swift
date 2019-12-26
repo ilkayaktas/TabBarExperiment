8e240665237490e42df49ae0242fc1b89a377e7a
İlkay Aktaş <ilkayaktas@gmail.com>
2019-12-26 22:54:54 +0300

    Initial Commit

---
+++ TabExperiment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabExperiment
+//
+//  Created by İlkay Aktaş on 26.12.2019.
+//  Copyright © 2019 İlkay Aktaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
